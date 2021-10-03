88f9ecac8181bf1b1c617679a098afc3332cc4ad
v-atamanenko <v@atamanenko.in>
2021-10-04 01:10:49 +0400

    Fix build script to install libs in ./tdlib folder; remove redundant build_native copying

--- build.sh
+++ build.sh
@@ -8,13 +8,15 @@ export CMAKE_PREFIX_PATH=/usr/local/vitasdk/arm-vita-eabi/
 
 echo "STAGE: Configuring compiler for cross-compilation"
 
-cmake -D__vita__=True -DCMAKE_BUILD_TYPE=Release -DCMAKE_CROSSCOMPILING=True -DOPENSSL_INCLUDE_DIR=/usr/local/vitasdk/arm-vita-eabi/include/ -DOPENSSL_LIBRARIES="/usr/local/vitasdk/arm-vita-eabi/lib/libssl.a;/usr/local/vitasdk/arm-vita-eabi/lib/libcrypto.a" ..
+cmake -D__vita__=True -DCMAKE_BUILD_TYPE=Release -DCMAKE_CROSSCOMPILING=True -DOPENSSL_INCLUDE_DIR=/usr/local/vitasdk/arm-vita-eabi/include/ -DOPENSSL_LIBRARIES="/usr/local/vitasdk/arm-vita-eabi/lib/libssl.a;/usr/local/vitasdk/arm-vita-eabi/lib/libcrypto.a" -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
 
 echo "STAGE: Compilation"
-rm -rf tdutils/generate
-cp -r ../build_native/tdutils/generate tdutils/
-rm -rf td/generate
-cp -r ../build_native/td/generate td/
+#rm -rf tdutils/generate
+#cp -r ../build_native/tdutils/generate tdutils/
+#rm -rf td/generate
+#cp -r ../build_native/td/generate td/
+mkdir -p ./td/telegram/
+cp ../build_native/td/telegram/tdjson_export.h ./td/telegram/
 
 
-cmake --build .
+cmake --build . --target install
